1de8749216d2a14c243468d58c27c100f412efc6
Mike Mason <github@mikemrm.com>
2025-04-19 22:19:14 -0500

    feat: ensure only a single scrape happens at a time

--- cmd/exporter.go
+++ cmd/exporter.go
@@ -3,6 +3,8 @@ package cmd
 import (
 	"log"
 	"net/http"
+	"sync"
+	"time"
 
 	"github.com/mikemrm/masscan-exporter/internal/exporter"
 	"github.com/mikemrm/masscan-exporter/internal/masscan"
@@ -29,7 +31,45 @@ func runExporter(cmd *cobra.Command, _ []string) {
 
 	mux := http.NewServeMux()
 
-	mux.Handle("/metrics", promhttp.HandlerFor(registry, promhttp.HandlerOpts{}))
+	var (
+		inFlightMu sync.RWMutex
+		inFlight   bool
+	)
+
+	isInFlight := func() bool {
+		inFlightMu.RLock()
+		defer inFlightMu.RUnlock()
+
+		return inFlight
+	}
+
+	setInFlight := func(v bool) {
+		inFlightMu.Lock()
+		defer inFlightMu.Unlock()
+
+		inFlight = v
+	}
+
+	mux.Handle("/metrics", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if isInFlight() {
+			log.Println("request already in flight")
+
+			w.WriteHeader(http.StatusTooManyRequests)
+
+			return
+		}
+
+		setInFlight(true)
+		defer setInFlight(false)
+
+		s := time.Now()
+
+		log.Println("starting request")
+
+		promhttp.HandlerFor(registry, promhttp.HandlerOpts{}).ServeHTTP(w, r)
+
+		log.Printf("request completed in %s", time.Since(s))
+	}))
 
 	log.Printf("Listening on %s", cfg.Server.Listen)
 
